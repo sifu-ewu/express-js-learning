21bc96e2a2c697284dd9c5107cf3edcaeafe6cf1
sifu-ewu <yotullah@gmail.com>
2025-03-24 14:53:25 +0600

    updated

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const { format } = require('date-fns');
 const port = process.env.PORT || 3000;
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Simple route for the home page
-app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to my Express.js learning project');
+// Logging middleware
+app.use(function(req, res, next) {
+  console.log("Time:", Date.now());
+  next();
+});
+
+app.use(function(req, res, next) {
+  const formatted = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+  console.log(formatted); 
+  next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/api", (req, res, next) => {
+  if (true) {
+    return next(new Error("Something broke!"));
+  }
+  res.send("Hello World");
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Not Implemented!");
 });
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log(`Server running on http://localhost:3000`);
 });
